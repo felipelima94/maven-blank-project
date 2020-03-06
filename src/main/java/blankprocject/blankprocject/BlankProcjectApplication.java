8fe16a983752527308162a320c974db560444f47
Felipe Lima <felipe.lima@solinftec.com.br>
2020-03-06 17:56:54 -0300

    first commit

---
+++ src/main/java/blankprocject/blankprocject/BlankProcjectApplication.java
@@ -0,0 +1,13 @@
+package blankprocject.blankprocject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlankProcjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlankProcjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/blankprocject/blankprocject/BlankProcjectApplicationTests.java
@@ -0,0 +1,13 @@
+package blankprocject.blankprocject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlankProcjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
