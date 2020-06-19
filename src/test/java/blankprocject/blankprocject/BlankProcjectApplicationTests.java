bcdfe06e41790a528b161ca54c70107968fe069d
Felipe Lima <felipe.lima@solinftec.com.br>
2020-06-19 18:24:07 +0000

    removendo dependencias sem uso

--- src/test/java/blankprocject/blankprocject/BlankProcjectApplicationTests.java
+++ src/test/java/blankprocject/blankprocject/BlankProcjectApplicationTests.java
@@ -1,11 +1,9 @@
 package blankprocject.blankprocject;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.*;
 import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 class BlankProcjectApplicationTests {
 
